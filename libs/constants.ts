05ba075ea99e045b94501bcc6181c806c372a562
Marco Aurelio Maturana Silveira <marco.maturana.ro@gmail.com>
2019-04-16 16:16:27 -0400

    constants with decorators names

---
+++ libs/constants.ts
@@ -0,0 +1,10 @@
+
+export const AUTHENTICATION_DECORATOR = "__authentication-decorator__";
+
+export const AUTHORIZATION_DECORATOR = "__authorization-decorator__";
+
+export const MIDDLEWARES_DECORATOR = "__middlewares-decorator__";
+
+export const ROUTE_DECORATOR = "__route-decorator__";
+
+export const SWAGGER_DECORATOR = "__swagger-decorator__";
