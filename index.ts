84f6a9c0388f59bd674f439f30d9e30ffb91b0da
Marco Aurelio Maturana Silveira <marco.maturana.ro@gmail.com>
2019-04-22 17:11:51 -0400

    index file to export the library

--- index.ts
+++ index.ts
@@ -1,7 +1,31 @@
 import "reflect-metadata";
 
-import { Authentication } from "./libs/decorator-authentication";
+import Authentication from "./libs/decorator-authentication";
+import Middleware from "./libs/decorator-middleware";
+import {
+  All,
+  Delete,
+  Get,
+  Head,
+  Options,
+  Patch,
+  Post,
+  Put
+} from "./libs/decorator-request";
+import Route from "./libs/decorator-route";
+import RouteExplorer from "./libs/route-explorer";
 
 export {
-  Authentication
+  All,
+  Authentication,
+  Delete,
+  Get,
+  Head,
+  Middleware,
+  Options,
+  Patch,
+  Post,
+  Put,
+  Route,
+  RouteExplorer
 }
